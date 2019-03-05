8e90e842f1989d91422c02b94157f2b6ebce17e1
juancebolla <cristian.cornejo@usach.cl>
2019-03-05 11:15:26 -0300

    Update ListarDesafiosService.java
    FacesContext

--- src/main/java/cl/ares/bice/service/ListarDesafiosService.java
+++ src/main/java/cl/ares/bice/service/ListarDesafiosService.java
@@ -19,7 +19,8 @@ import java.io.InputStream;
 import java.util.List;
 import java.util.Properties;
 
-
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -28,9 +29,9 @@ import javax.naming.InitialContext;
 public class ListarDesafiosService {
 	
 	public ListarDesafiosResponse listarDesafios(ListarDesafios parameters){
-		System.out.println("LISTAR DESAFIOS POST PRUEBA");
-		String fileName = System.getProperty("jboss.server.config.dir");
-		System.out.println("fileName:"+fileName);
+		System.out.println("LISTAR DESAFIOS POST PRUEBA 05032019");
+		//String fileName = System.getProperty("jboss.server.config.dir");
+		//System.out.println("fileName:"+fileName);
 		System.out.println("entrada.usuario:"+parameters.getUsuarioId());
 		System.out.println("entrada.tipo:"+parameters.getTipoCliente());
         Tools t = new Tools();
@@ -45,6 +46,10 @@ public class ListarDesafiosService {
         Properties prop = new Properties();
         //Carga SMS PROPERTIES
         try {
+			FacesContext context = FacesContext.getCurrentInstance();
+			ServletContext servletContext = (ServletContext) context.getExternalContext().getContext();
+			String pathReal = servletContext.getRealPath("/");
+			System.out.println("pathReal:"+pathReal);
             env = (Context)new InitialContext().lookup("java:comp/env");
             file = (String)env.lookup("TipoCliente.props");
             input = new FileInputStream(file);
